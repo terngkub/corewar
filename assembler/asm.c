6f5fc020f1722e77cff55d1379650c5640437558
terngkub <terngnattapol@gmail.com>
2018-02-05 21:07:23 +0100

    read and write .cor file

--- assembler/asm.c
+++ assembler/asm.c
@@ -5,34 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkamolba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/05 18:39:12 by nkamolba          #+#    #+#             */
-/*   Updated: 2018/02/05 20:48:15 by fbabin           ###   ########.fr       */
+/*   Created: 2018/02/05 19:15:02 by nkamolba          #+#    #+#             */
+/*   Updated: 2018/02/05 21:06:26 by nkamolba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-/*void	read_input(char *file_name)
+void	ft_error(char *str)
 {
-	int		fd;
-	char	*str;
+	ft_printf("ERROR: %s", str);
+	exit(1);
+}
 
-	fd = open(file_name, O_RDONLY);
-	while (get_next_line(fd, &str) > 0)
-	{
-		ft_printf("test\n");
-	}
-	close(fd);
-}*/
+char	*get_cor_name(char *filename)
+{
+	int		len;
+	char	*cor_filename;
+
+	len = ft_strlen(filename);
+	if (!(cor_filename = ft_strnew(len + 2)))
+		ft_error("malloc failed");
+	ft_strncpy(cor_filename, filename, len - 1);
+	ft_strcat(cor_filename, "cor");
+	return (cor_filename);
+}
+
+int		check_file_name(char *str)
+{
+	int		len;
+
+	len = ft_strlen(str);
+	if (len > 2 && str[len - 2] == '.' && str[len - 1] == 's' && str[len] == 0)
+		return (1);
+	return (0);
+}
 
 int		main(int argc, char **argv)
 {
-	(void)argv;
-	if (argc < 2)
+	int		fd_read;
+	int		fd_write;
+	char	*cor_filename;
+	char	*line;
+
+	if (argc != 2 || !check_file_name(argv[1]))
+		ft_error("wrong input");
+	fd_read = open(argv[1], O_RDONLY);
+	cor_filename = get_cor_name(argv[1]);
+	fd_write = open(cor_filename, O_WRONLY | O_CREAT, 0755);
+	while (get_next_line(fd_read, &line) > 0)
 	{
-		ft_printf("ERROR");
-		return (1);
+		ft_putstr_fd(line, fd_write);
+		ft_putstr_fd("\n", fd_write);
+		free(line);
 	}
-	//read_input(argv[argc - 1]);
+	close(fd_read);
+	close(fd_write);
 	return (0);
 }
