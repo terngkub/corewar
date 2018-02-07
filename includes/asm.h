dd40736ed1e97aa21ce0db2acc652c6ab1b4c305
fxbabin <fbabin@student.42.fr>
2018-02-07 21:12:34 +0100

    writing name and comment in bytecode

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: nkamolba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 18:40:36 by nkamolba          #+#    #+#             */
-/*   Updated: 2018/02/07 18:14:21 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/02/07 20:55:58 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,14 @@
 /*
 ** -------------------------------- STRUCTURES --------------------------------
 */
+
+typedef struct	s_champ
+{
+	char		name[PROG_NAME_LENGTH + 1];
+	char		comment[COMMENT_LENGTH + 1];
+	t_list		*instructions;
+	t_list		*labels;
+}				t_champ;
 
 typedef struct	s_op
 {
@@ -51,7 +59,7 @@ void			ft_error(char *str);
 ** ------------------------------ CHECK FUNCTIONS ------------------------------
 */
 
-int			check_name(char *line, int line_nb);
-int			check_comment(char *line, int line_nb);
+int			check_name(t_champ *champ, char *line, int line_nb);
+int			check_comment(t_champ *champ, char *line, int line_nb);
 
 #endif
