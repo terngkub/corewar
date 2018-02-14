36a122c482dd988e169df21acb034da0aa21cae1
arobion <arobion@student.42.fr>
2018-02-14 20:19:34 +0100

    end of the day for me(arobion)

--- V_M/srcs/main.c
+++ V_M/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: arobion <arobion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 15:18:27 by arobion           #+#    #+#             */
-/*   Updated: 2018/02/14 20:07:06 by arobion          ###   ########.fr       */
+/*   Updated: 2018/02/14 20:15:20 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int		main(int argc, char **argv)
 	if (!(load_champs(arn, argv, nb_players)))
 		return (0);
 	init_arena(&arn, nb_players, argv);
-	//print_arena(arn);
-	//print_mem(arn.mem, MEM_SIZE);
+	print_arena(arn);
+	print_mem(arn.mem, MEM_SIZE);
 	return (0);
 }
