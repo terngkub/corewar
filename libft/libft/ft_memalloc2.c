1e70b88762685b9f7d293adc4c80c6d75b68fefa
fxbabin <fbabin@student.42.fr>
2018-02-16 22:33:31 +0100

    corrected some norm errors

--- libft/libft/ft_memalloc2.c
+++ libft/libft/ft_memalloc2.c
@@ -6,21 +6,11 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 16:54:05 by fbabin            #+#    #+#             */
-/*   Updated: 2018/01/27 17:12:47 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/02/16 22:30:53 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_free2(void **tab)
-{
-	int		i;
-
-	i = -1;
-	while (tab[++i])
-		free(tab[i]);
-	free(tab);
-}
 
 void		**ft_memalloc2(size_t si1, size_t si2, size_t size)
 {
