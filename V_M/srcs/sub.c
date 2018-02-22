8732d1e2766776f3cd71d6aa9bcdd13e7cce6bfc
arobion <arobion@student.42.fr>
2018-02-22 16:45:33 +0100

    add/sub

--- V_M/srcs/sub.c
+++ V_M/srcs/sub.c
@@ -6,7 +6,7 @@
 /*   By: arobion <arobion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 21:23:14 by arobion           #+#    #+#             */
-/*   Updated: 2018/02/21 18:48:09 by arobion          ###   ########.fr       */
+/*   Updated: 2018/02/22 16:40:06 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,43 @@ int		check_ocp_sub(char ocp)
 	return (1);
 }
 
-int		check_sub_param(t_arena *arn, t_process *process)
+static int		change_move_pc(int val)
+{
+	if (val == 0)
+		return (0);
+	if (val == 1)
+		return (1);
+	if (val == 2)
+		return (4);
+	if (val == 3)
+		return (2);
+	return (0);
+}
+
+static int		modif_pc(unsigned char ocp)
+{
+	int				ret;
+	unsigned char	comp;
+
+	ret = 0;
+	comp = ocp >> 6;
+	ret += change_move_pc(comp);
+	comp = ocp << 2;
+	comp = comp >> 6;
+	ret += change_move_pc(comp);
+	comp = ocp << 4;
+	comp = comp >> 6;
+	ret += change_move_pc(comp);
+	return (ret);
+}
+
+int		check_sub_param(t_arena *arn, t_process *process, int *move_pc)
 {
 	if (check_ocp_sub(arn->mem[process->pc]) == 0)
+	{
+		*move_pc = modif_pc(arn->mem[process->pc]);
 		return (0);
+	}
 	if (arn->mem[(process->pc + 1) % MEM_SIZE] > REG_NUMBER || arn->mem[(process->pc + 1) % MEM_SIZE] < 1)
 		return (0);
 	if (arn->mem[(process->pc + 2) % MEM_SIZE] > REG_NUMBER || arn->mem[(process->pc + 2) % MEM_SIZE] < 1)
@@ -50,10 +83,14 @@ int		check_sub_param(t_arena *arn, t_process *process)
 void	sub(t_arena *arn, t_process *process)
 {
 	int		res;
+	int		move_pc;
 
 	process->pc = (process->pc + 1) % MEM_SIZE;
-	if (!(check_sub_param(arn, process)))
+	if (!(check_sub_param(arn, process, &move_pc)))
+	{
+		process->pc = (process->pc + 1) % MEM_SIZE;
 		return ;
+	}
 	res = x_char_to_int(process->regs[arn->mem[(process->pc + 1)\
 			% MEM_SIZE] - 1], REG_SIZE);
 	res -= x_char_to_int(process->regs[arn->mem[(process->pc + 2)\
