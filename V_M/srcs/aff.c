28413d30040bf2d8c6ff833827afac7157271b36
arobion <arobion@student.42.fr>
2018-02-22 14:54:27 +0100

    move of pc corrected

--- V_M/srcs/aff.c
+++ V_M/srcs/aff.c
@@ -6,7 +6,7 @@
 /*   By: arobion <arobion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 19:57:25 by arobion           #+#    #+#             */
-/*   Updated: 2018/02/17 16:16:23 by arobion          ###   ########.fr       */
+/*   Updated: 2018/02/22 14:50:12 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,29 @@ void	ft_print_reg(char *str)
 	}
 }
 
-int		verif_ocpar_aff(t_process *process, t_arena *arn)
+int		change_move_pc(int val)
 {
-	int		bit_value;
-	int		i;
+	if (val == 0)
+		return (0);
+	if (val == 1)
+		return (1);
+	if (val == 2)
+		return (4);
+	if (val == 3)
+		return (2);
+	return (0);
+}
 
-	i = (process->pc + 1) & MEM_SIZE;
-	bit_value = arn->mem[i] >> 6;
+int		verif_ocpar_aff(t_process *process, t_arena *arn, int *j)
+{
+	unsigned char	bit_value;
+	int				i;
+	unsigned char	test;
+
+	i = (process->pc + 1) % MEM_SIZE;
+	test = arn->mem[i];
+	bit_value = test >> 6;
+	*j = change_move_pc(bit_value);
 	if (bit_value == 1)
 		return (1);
 	else
@@ -41,9 +57,11 @@ void	aff(t_arena *arn, t_process *process)
 {
 	int		to_aff;
 	int		pos;
+	int		move_pc;
 
+	move_pc = 0;
 	pos = arn->mem[(process->pc + 2) % MEM_SIZE] - 1;
-	if (verif_ocpar_aff(process, arn) == 1 && pos < REG_NUMBER)
+	if (verif_ocpar_aff(process, arn, &move_pc) == 1 && (pos < REG_NUMBER && pos >= 0))
 	{
 		to_aff = x_char_to_int(process->regs[pos], REG_SIZE);
 		to_aff %= 256;
@@ -57,5 +75,5 @@ void	aff(t_arena *arn, t_process *process)
 			process->carry = 0;
 	}
 	else
-		process->pc += 1;
+		process->pc = (process->pc + 2 + move_pc) % MEM_SIZE;
 }
