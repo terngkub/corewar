ca6de2448b92cc9f8948ec5d8490022e9bbe495f
arobion <arobion@student.42.fr>
2018-02-22 15:20:12 +0100

    corrected move of pc

--- V_M/srcs/st.c
+++ V_M/srcs/st.c
@@ -6,13 +6,13 @@
 /*   By: arobion <arobion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 11:11:47 by arobion           #+#    #+#             */
-/*   Updated: 2018/02/21 22:04:12 by nkamolba         ###   ########.fr       */
+/*   Updated: 2018/02/22 15:16:31 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-int			check_ocp_st(unsigned char ocp)
+int			valid_ocp(unsigned char ocp)
 {
 	unsigned char	comp;
 
@@ -26,6 +26,47 @@ int			check_ocp_st(unsigned char ocp)
 	if (comp == 3)
 		return (1);
 	return (-1);
+}
+
+static int		change_move_pc(int val)
+{
+	if (val == 0)
+		return (0);
+	if (val == 1)
+		return (1);
+	if (val == 2)
+		return (4);
+	if (val == 3)
+		return (2);
+	return (0);
+}
+
+int			modif_pc(unsigned char ocp)
+{
+	int				ret;
+	unsigned char	comp;
+
+	ret = 0;
+	comp = ocp >> 6;
+	ret += change_move_pc(comp);
+	comp = ocp << 2;
+	comp = comp >> 6;
+	ret += change_move_pc(comp);
+	return (ret);
+}
+
+int			check_ocp_st(unsigned char ocp, int *j)
+{
+	int				i;
+
+	*j = 0;
+	if ((i = valid_ocp(ocp)) == -1)
+	{
+		*j = modif_pc(ocp);
+		return (-1);
+	}
+	else
+		return (i);
 }
 
 int			check_rg(int value)
@@ -66,11 +107,15 @@ void		st2(t_arena *arn, t_process *process)
 	int		i;
 	int		rg;
 	int		id;
+	int		move_pc;
 
 	process->pc = (process->pc + 1) % MEM_SIZE;
-	i = check_ocp_st(arn->mem[process->pc]);
+	i = check_ocp_st(arn->mem[process->pc], &move_pc);
 	if (i == -1)
+	{
+		process->pc = (process->pc + 1 + move_pc) % MEM_SIZE;
 		return ;
+	}
 	if (!(check_rg(arn->mem[(process->pc + 1) % MEM_SIZE])))
 		return ;
 	if (i == 0)
